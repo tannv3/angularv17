ae2b8050a3bf90a46a1711b2411ec86765801674
tannv3 <tannv3@nal.vn>
2025-04-25 14:46:50 +0700

    🔄 Sync leasing/src from angular14@61eb06e

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular_v17'`, () => {
+  it(`should have as title 'angular_v14'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_v17');
+    expect(app.title).toEqual('angular_v14');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_v17 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_v14 app is running!');
   });
 });
